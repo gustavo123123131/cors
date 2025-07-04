ef03405ae16bf637b477106504d1ec286d943b13
gustavo123123131 <gustabarbosa1008@gmail.com>
2025-07-04 18:44:16 -0300

    Update location.js

--- api/location.js
+++ api/location.js
@@ -1,11 +1,16 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
   try {
-    // Chama a API antiga
     const response = await fetch("https://ipapi.co/json/");
     const data = await response.json();
-
-    // Libera CORS
-    res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: "Erro ao buscar localização." });
